5aeeaa1d2e1edd2462b8d295063882f1a6e4ca46
yindaheng98 <yindaheng98@163.com>
2020-05-06 21:00:25 +0800

    bug fix

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,8 +1,12 @@
 <template>
-    <div class="user-info">
+    <div>
         <label>
-            用户名：<input v-model="ID" type="text" placeholder="用户名">
-            密码：<input v-model="PASS" type="password" placeholder="密码">
+            用户名：
+            <input v-model="ID" type="text" placeholder="用户名">
+            <span v-if="IDValid">ok</span>
+            密码：
+            <input v-model="PASS" type="password" placeholder="密码">
+            <span v-if="PASSValid">ok</span>
         </label>
     </div>
 </template>
@@ -12,7 +16,28 @@
         name: "UserInfo",
         data: function () {
             return {ID: "", PASS: ""}
+        },
+        computed: {
+            /**
+             * @return {boolean}
+             */
+            IDValid() {
+                return Valid(this.$data.ID)
+            },
+            /**
+             * @return {boolean}
+             */
+            PASSValid() {
+                return Valid(this.$data.PASS)
+            }
         }
+    }
+
+    /**
+     * @return {boolean}
+     */
+    function Valid(str) {
+        return str.length >= 6 && !RegExp(/[^A-Za-z0-9]/).test(str)
     }
 </script>
 
