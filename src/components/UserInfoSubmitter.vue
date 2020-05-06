ab0211ae33ca22705f169b5911a9b7fe7e9410ed
yindaheng98 <yindaheng98@163.com>
2020-05-06 21:11:49 +0800

    提交组件完成

--- src/components/UserInfoSubmitter.vue
+++ src/components/UserInfoSubmitter.vue
@@ -1,11 +1,9 @@
 <template>
     <div>
         <UserInfo ref="UserInfo"/>
-        <transition name="button">
-            <button @click="Submit()" v-if="!waiting">{{ buttonName }}</button>
-        </transition>
+        <button @click="Submit()" :disabled="waiting">{{ buttonName }}</button>
         <transition name="waiting">
-            <div ref="waiting" v-if="waiting">等待中</div>
+            <div v-if="waiting">等待中</div>
         </transition>
     </div>
 </template>
@@ -21,10 +19,13 @@
             verify: Function,//用于提交验证的函数，返回true/false
             received: Function,//当提交成功时调用，接收verify返回的true/false
             rejected: Function,//当提交失败时调用
-            errorred: Function,//当提交出错时调用
+            errored: Function,//当提交出错时调用
         },
         methods: {
             Submit() {
+                let UserInfo = this.$refs.UserInfo;
+                if (!UserInfo.IDValid) return alert("非法用户名");
+                if (!UserInfo.PASSValid) return alert("非法密码");
                 let ID = this.$refs.UserInfo.$data.ID;//取ID
                 let PASS = this.$refs.UserInfo.$data.PASS;//取PASS
                 this.$data.waiting = true;//开始等待
@@ -33,7 +34,7 @@
                     this.$props.received(result);//并调用接收完成函数
                 }).catch((e) => {
                     this.$data.waiting = false;//出错时结束等待
-                    this.$props.errorred(e)//并调用出错处理函数
+                    this.$props.errored(e)//并调用出错处理函数
                 })
             }
         },
