23524a447c77074da606272401d787694639849a
yindaheng98 <yindaheng98@163.com>
2020-05-18 16:40:36 +0800

    用户信息输入框样式调节完成

--- src/components/LoginPage/UserInfo.vue
+++ src/components/LoginPage/UserInfo.vue
@@ -1,13 +1,15 @@
 <template>
-    <div>
-        <label>
-            用户名：
-            <input v-model="ID" type="text" placeholder="用户名">
-            <span v-if="IDValid">ok</span>
-            密码：
-            <input v-model="PASS" type="password" placeholder="密码">
-            <span v-if="PASSValid">ok</span>
-        </label>
+    <div class="content">
+        <div class="input-content">
+            <div class="text-label"><label for="ID" class="label-body">用户名</label></div>
+            <input class="text-input" v-model="ID" type="text" placeholder="用户名" id="ID">
+            <div class="ok"><img v-if="IDValid" height="30" alt="ok" src="../../assets/ok.svg"></div>
+        </div>
+        <div class="input-content">
+            <div class="text-label"><label for="PASS" class="label-body">密码</label></div>
+            <input class="text-input" v-model="PASS" type="password" placeholder="密码" id="PASS">
+            <div class="ok"><img v-if="PASSValid" height="30" alt="ok" src="../../assets/ok.svg"></div>
+        </div>
     </div>
 </template>
 
@@ -42,5 +44,33 @@
 </script>
 
 <style scoped>
+    .content, .input-content {
+        width: 300px;
+    }
 
+    .input-content {
+        text-align: center;
+    }
+
+    .text-label {
+        width: 72px;
+        display: inline;
+        text-align: right;
+        float: left;
+    }
+
+    .label-body {
+        font-size: 2.4rem;
+        font-weight: 300;
+    }
+
+    .ok {
+        width: 30px;
+        height: 30px;
+        float: right;
+    }
+
+    .text-input {
+        display: inline;
+    }
 </style>
